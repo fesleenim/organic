1d5e1459f71cde73ca4cac937a421bd0cfd5afe4
Fesleen <mosajonovae@gmail.com>
2025-01-25 17:06:08 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import Header from './component/header/Header'
-import About from './component/about/About'
-import Hero from './component/hero/Hero'
-import Product from './component/product/Product'
-import Comment from './component/comment/Comment'
-import News from './component/news/News'
-import Footer from './component/footer/Footer'
-import Offer from './component/offer/Offer'
+import Header from './Components/Header/Header'
+// import About from './component/about/About'
+// import Hero from './component/hero/Hero'
+// import Product from './component/product/Product'
+// import Comment from './component/comment/Comment'
+// import News from './component/news/News'
+// import Footer from './component/footer/Footer'
+// import Offer from './component/offer/Offer'
 
 const App = () => {
 
   return (
    <>
    <Header/>
-   <Hero/>
+   {/* <Hero/>
    <About/>
    <Product/>
    <Comment/>
    <Offer/>
    <News/>
-   <Footer/>
+   <Footer/> */}
    </>
   )
 }
