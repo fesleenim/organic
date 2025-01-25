d4b8131d03af3d956bb9300097c6acdf7626a72a
Fesleen <mosajonovae@gmail.com>
2025-01-25 17:01:28 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './component/header/header'
-import About from './component/about/about'
-import Hero from './component/hero/hero'
-import Product from './component/product/product'
-import Comment from './component/comment/comment'
-import News from './component/news/news'
-import Footer from './component/footer/footer'
-import Offer from './component/offer/offer'
+import Header from './component/header/Header'
+import About from './component/about/About'
+import Hero from './component/hero/Hero'
+import Product from './component/product/Product'
+import Comment from './component/comment/Comment'
+import News from './component/news/News'
+import Footer from './component/footer/Footer'
+import Offer from './component/offer/Offer'
 
 const App = () => {
 
